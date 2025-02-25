eac8edf218ac432c9e7b861730c97deff980106d
Jose.zaldivar <joseluiszaldivaralmeida@gmail.com>
2025-02-24 20:17:04 -0500

    Setting

--- peluqueriaBack/settings.py
+++ peluqueriaBack/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 # Actual directory user files go to
 MEDIA_ROOT = BASE_DIR / 'media'
 
